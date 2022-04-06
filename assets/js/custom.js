b48b671001a1cc5c4703f75e9d084e3ea7519a59
cldevHS <web.passion.dev@gmail.com>
2022-04-07 03:50:35 +0900

    update files

---
+++ assets/js/custom.js
@@ -0,0 +1,36 @@
+// =============================== Modals =================================
+$(document).ready(function(){
+
+  $('.modal__inner').fadeOut();
+
+  $('.modal-view-btn').on('click', function(){
+    $('#modal').addClass('visible');
+  });
+
+  $('.mask').on('click', function(){
+    $('.modal-wrap').removeClass('visible');
+  });
+
+  var modalWidth = 656;
+  var modalHeight;
+
+  setDimensions();
+
+  function setDimensions() {
+    modalHeight = $(window).innerHeight() - 120;
+    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+     modalWidth = $(window).innerWidth() - 30;
+     modalHeight = $(window).innerHeight() - 30;
+    }
+    $('.modal').css('max-width', modalWidth);
+    $('.modal').css('max-height', modalHeight);
+  }
+  $('.close-btn').on('click', function(){
+    $('.modal-wrap').removeClass('visible');
+  });
+
+  $('.crossBtn').on('click', function(){
+    $('.modal-wrap').removeClass('visible');
+  });
+});
+// ============================== End of Modals ===================================
